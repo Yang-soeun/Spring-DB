54159ed564e68b5a69d5f4c8bf6cb226602f9744
YangSoeun <soeun8636@naver.com>
2024-07-20 19:11:18 +0900

    [FEAT]: 예외 포함과 스택 트레이스

--- jdbc/src/test/java/db/jdbc/exception/basic/UnCheckedAppTest.java
+++ jdbc/src/test/java/db/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,17 +1,30 @@
 package db.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Slf4j
 public class UnCheckedAppTest {
     @Test
     void unchecked(){
         Controller controller = new Controller();
         assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(RuntimeSQLException.class);
+    }
+
+    @Test
+    void printEx(){
+        Controller controller = new Controller();
+        try{
+            controller.request();
+        } catch (Exception e){
+//            e.printStackTrace(); // 이렇게 하는 것 보다 로그를 남기는 방법이 좋음, 이 방식은 System.out을 사용함
+            log.info("ex", e);  // 로그를 출력할 때 마지막 파라미터에 예외를 넣어주면 로그에 스택 트레이스를 출력할 수 있다. or 예외만 파라미터에 전달
+        }
     }
 
     static class Controller {
@@ -44,6 +57,7 @@ public class UnCheckedAppTest {
                 runSQL();
             } catch (SQLException e) {
                 throw new RuntimeSQLException(e);  //런타임 예외로 바꿔서 던짐
+//                throw new RuntimeSQLException();  // 이런식으로 e를 전달하지 않으면 무슨 이유때문에 RuntimeException이 발생했는지 알 수 없다.
             }
         }
 
@@ -59,7 +73,7 @@ public class UnCheckedAppTest {
     }
 
     static class RuntimeSQLException extends RuntimeException{
-        public RuntimeSQLException(Throwable cause){
+        public RuntimeSQLException(Throwable cause){    // 이렇게 하면 내부에 기존 exception을 가지고 있음
             super(cause);
         }
     }
